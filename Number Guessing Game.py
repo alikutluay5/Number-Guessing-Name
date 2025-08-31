fce763b5c2ff08d061c26a7b9b509aca0dde46cb
alikutluay5 <alikutluay543@gmail.com>
2025-08-31 12:24:53 +0300

    Add files via upload

---
+++ Number Guessing Game.py
@@ -0,0 +1,24 @@
+import random
+
+print('Hello! This is a basic number guessing game')
+number = random.randint(1, 100)
+tries = 5
+
+while tries > 0:
+    guess = int(input('Guess a number between 1 and 100: '))
+    
+    if guess < number:
+        tries -= 1
+        print('Wrong! The number is higher than your guess.')
+        print('Your tries left:', tries)
+    elif guess > number:
+        tries -= 1
+        print('Wrong! The number is lower than your guess.')
+        print('Your tries left:', tries)
+    else:
+        print('Congratulations! You guessed the correct number:', number)
+        break  # Doğru tahmin olursa döngüden çık
+    
+    if tries == 0:
+        print('Game over! The number was:', number)
+        print('Better luck next time!')
